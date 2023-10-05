f19050fdf5d333b1b052e62b0229445dc0b3b077
Wandrille Dioubaté <74004697+wandrilledioubate@users.noreply.github.com>
2023-10-05 10:30:06 +0200

    Initialization of the code architecture 🚧

---
+++ eye-cancer-front/src/components/pages/Chatbot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+
+function Chatbot() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Chatbot;
